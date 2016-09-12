736aecfe012929b2fc4cad4d73a3302ff2f93782
MogilevichDmitry <dmsmain@gmail.com>
2016-09-12 22:21:55 +0300

    Implement toggle active class to change-view-buttons

--- js/common.js
+++ js/common.js
@@ -1,4 +1,6 @@
 (function() {
+
+    /* map actions*/
     var mapButton = document.getElementById('map-expand');
     var mapBox = document.getElementById('map-box');
     var contentSearchMap = document.getElementById('content__search-map');
@@ -6,5 +8,16 @@
     mapButton.onclick = function() {
         contentSearchMap.classList.toggle('content__search-map_row');
         mapBox.classList.toggle('map-box_expanded');
+        mapButton.classList.toggle('fullscreen-btn_active');
+    }
+
+    /* change view buttons */
+    var viewButton = document.getElementsByClassName('view-selector__item');
+
+    for (var i = 0; i < viewButton.length; i++) {
+        viewButton[i].onclick = function () {
+            viewButton[0].classList.toggle('view-selector__item_active');
+            viewButton[1].classList.toggle('view-selector__item_active');
+        }
     }
 })();
