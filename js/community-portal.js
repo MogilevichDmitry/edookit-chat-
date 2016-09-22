3008d496e5a44421ea6c076a7387fa4d58cdb075
MogilevichDmitry <dmsmain@gmail.com>
2016-09-22 14:15:35 +0300

    Add account counter to community portal, Implement toggle account expand block

--- js/community-portal.js
+++ js/community-portal.js
@@ -1,4 +1,6 @@
 (function() {
+
+    /* toggle menu */
     var menuBtn = document.getElementById('menu-btn');
     var menuList = document.getElementById('menu-list');
 
@@ -6,4 +8,11 @@
        menuList.classList.toggle('menu-list_closed');
     }
 
+    /* toggle account-expand */
+    var accountExpandBtn = document.getElementById('account-expand-btn');
+    var accountExpandBlock = document.getElementById('account-expand-block');
+    accountExpandBtn.onclick = function() {
+        accountExpandBtn.classList.toggle('expand-btn_active')
+        accountExpandBlock.classList.toggle('_no-visible');
+    }
 })();
